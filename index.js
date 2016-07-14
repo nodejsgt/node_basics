b8226b788a5427b99515df7af1ef5621453b5cf1
unknown <30202775@gua-hcp-lt258.acshcp.com>
2016-07-14 10:18:13 -0600

    step-0

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+Problema: Crear una aplicación de consola que nos permita consultar la base de datos de omdbapi.com
+
+Solución: Se crearán un par de métodos que sirvan para mostrar la información obtenida y otra función
+          que nos permitirá consultar la información por medio del método http que provee node.js
+*/
